4476ee1e7973dc07221d0e3690259b98804e0cca
Mohmadaman Vhora <mohmadaman@vhora.com>
2023-03-06 17:59:09 +0530

    TE7-T117:Rails:Complete task 1 to 6 of practicals of active record migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_050933) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_122336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dob"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
