b1deea09e3fce92cfd3260f4cda57b687cdc179f
Mohmadaman Vhora <mohmadaman@vhora.com>
2023-03-07 19:10:54 +0530

    TE7-T117:Rails:Complete tasks 1 to 13 of active record migrations practicals

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_many :authors, dependent: :destroy
+  belongs_to :author, optional: true
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
